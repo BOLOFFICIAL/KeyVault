d11087b025231360936106b9bd0f951de37438da
BOLOFFICIAL <boloninnn@gmail.com>
2023-06-11 16:49:38 +0300

    Update

--- KeyVaultWindows/ViewModel/PageMainViewModel.cs
+++ KeyVaultWindows/ViewModel/PageMainViewModel.cs
@@ -13,16 +13,42 @@ using System.Reflection.Metadata;
 using System.Security.Policy;
 using System.Windows.Shapes;
 using System.Windows;
+using System.ComponentModel;
 
 namespace KeyVaultWindows.ViewModel
 {
-    class PageMainViewModel
+    class PageMainViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
         public ICommand PageTransition { get; }
+        private Main _main;
 
         public PageMainViewModel()
         {
             PageTransition = new LambdaCommand(OnPageTransitionCommandExecuted, CanPageTransitionCommandExecute);
+            _main = new Main();
+        }
+
+        public string FindPassword
+        {
+            get
+            {
+                return _main.FindPassword;
+            }
+            set
+            {
+                if (_main.FindPassword != value)
+                {
+                    _main.FindPassword = value;
+                    OnPropertyChanged("FindPassword");
+                }
+            }
+        }
+
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
         }
 
         private void OnPageTransitionCommandExecuted(object p) 
@@ -32,7 +58,7 @@ namespace KeyVaultWindows.ViewModel
                 case "PasswordManagement":
                     Context.management = new Management() 
                     { 
-                        IsReadonly = true, 
+                        IsReadonly = false, 
                         Title = "Добавить пароль", 
                         ButtonContent = "Добавить",
                         GridLength = new GridLength(0, GridUnitType.Star)
